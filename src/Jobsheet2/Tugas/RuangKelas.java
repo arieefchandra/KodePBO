e0f555ca89f8b44eb86a4b76c53292c165e622c5
alexsyareuu-web <alexsyareuu@gmail.com>
2026-09-09 05:51:52 +0700

    Tugaas 3

---
+++ src/Jobsheet2/Tugas/RuangKelas.java
@@ -0,0 +1,21 @@
+package Jobsheet2.Tugas;
+
+public class RuangKelas {
+    public String kodeRuang;
+    public String namaGedung;
+    public int kapasitas;
+    public int jumlahMahasiswa;
+
+    public int hitungSisaKursi() {
+        int sisaKursi = kapasitas - jumlahMahasiswa;
+        return sisaKursi;
+    }
+
+    public void tampilData() {
+    System.out.println("Kode Ruang       : " + kodeRuang);
+    System.out.println("Nama Gedung      : " + namaGedung);
+    System.out.println("Kapasitas        : " + kapasitas);
+    System.out.println("Jumlah Mahasiswa : " + jumlahMahasiswa);
+    System.out.println("Sisa Kursi       : " + hitungSisaKursi());
+    }
+}
