317b776e6792766151a066170425a937259f0229
alexsyareuu-web <alexsyareuu@gmail.com>
2026-09-18 08:19:55 +0700

    Percobaan 1

---
+++ src/Jobsheet4/Percobaan1/MainPercobaan1.java
@@ -0,0 +1,14 @@
+package Jobsheet4.Percobaan1;
+
+public class MainPercobaan1 {
+    public static void main(String[] args) {
+        Processor p1 = new Processor();
+        p1.setMerk("Intel i5");
+        p1.setCache(4);
+        Laptop l1 = new Laptop();
+        l1.setMerk("Thinkpas");
+        l1.setProc(p1);
+        l1.info();
+    }
+
+}
