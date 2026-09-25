680cca9c6cd194e150440a41d4f65a0184cc5ba7
alexsyareuu-web <alexsyareuu@gmail.com>
2026-09-26 06:02:22 +0700

    Percobaan2

---
+++ src/Jobsheet6/Percobaan2/ClassB.java
@@ -0,0 +1,17 @@
+package Jobsheet6.Percobaan2;
+
+public class ClassB {
+    private int z;
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    public void getNilaiZ(){
+        System.out.println("nilai z : " + z);
+    }
+
+    public void getJumlah (){
+        System.out.println("jumlah : " + (x + y + z));
+    }
+}
---
+++ src/Jobsheet6/Percobaan2/Main2.java
@@ -0,0 +1,13 @@
+package Jobsheet6.Percobaan2;
+
+public class Main2 {
+     public static void main(String[] args) {
+        ClassB hitung = new ClassB();
+            hitung.setX (20);  
+            hitung.setY (30);
+            hitung.setZ(5);
+            hitung.getNilai();
+            hitung.getNilaiZ();
+            hitung.getJumlah();
+    }
+}
