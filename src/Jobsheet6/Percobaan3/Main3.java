ffc20edfaabcb96b1c770fe709e23c5f4e509473
alexsyareuu-web <alexsyareuu@gmail.com>
2026-09-26 06:47:54 +0700

    Percobaan3

---
+++ src/Jobsheet6/Percobaan3/Main3.java
@@ -0,0 +1,11 @@
+package Jobsheet6.Percobaan3;
+
+public class Main3 {
+    public static void main(String[] args) {
+        Tabung tabung = new Tabung();
+        tabung.setSuperPhi(3.14);
+        tabung.setSuperR(10);
+        tabung.setT(3);
+        tabung.volume();
+    }
+}
